3a23969b9d0ca5315f82660a6af60b4780a13873
yuval gofer <92877436+YuvalGofer@users.noreply.github.com>
2022-12-15 21:46:41 +0200

    Section 3 - API Architecture

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -9,25 +10,36 @@ namespace API.Controllers
     [Route("api/[Controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var product = await _context.Products.ToListAsync();
-
+            var product = await _repo.GetProductsAsync();
             return Ok(product);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<ProductBrand>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+         [HttpGet("types")]
+        public async Task<ActionResult<ProductType>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
         }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # sports-ecommerce
-Section 2 summary
-We add three project:
-API
-Infrastructure
-Core
+Section 3 summary
+-Adding repository and interface
+-Adding repository methods
+-Extending products entity
+-Creating new migrations and configure it
+-Applying the migrations and creating database at app startup
+-Adding seed data
+-Adding the code  to get the product brands and product types
+-Eager loading of navigation prop.
