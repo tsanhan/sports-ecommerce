92ffe302742ebf680beea6ba0228592d82d6e9c0
yuval gofer <92877436+YuvalGofer@users.noreply.github.com>
2023-01-31 00:02:48 +0200

    Section 14\151 Adding an item to basket in the component

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # sports-ecommerce
 
-Section 14\150 Adding an item to basket in the component
-- Adding into "export class BasketService" a few more methods:
-1. mapProductItemToBasketItem
-2. addUpdateItem
-3. addItemToBasket
-4. createBasket
-to ensure the basket that we can add items to basket
+Section 14\151 Adding an item to basket in the component
+- Update product-item.html to get onClick to cart icon to add product
+- Adding consol.log for setBasket to print out to console that basket is updating
 
 
 
--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -17,7 +17,7 @@ export class BasketService {
   constructor(private http: HttpClient) { }
 
   getBasket(id: string) {
-    return this.http.get(this.baseUrl + 'basketId' + id)
+    return this.http.get(this.baseUrl + 'basket?id=' + id)
       .pipe(
         map((basket: IBasket) => {
           this.basketSource.next(basket);
@@ -26,8 +26,9 @@ export class BasketService {
   }
 
   setBasket(basket: IBasket) {
-    return this.http.get(this.baseUrl + 'basket' + basket).subscribe((response: IBasket) => {
+    return this.http.post(this.baseUrl + 'basket', basket).subscribe((response: IBasket)=>{
       this.basketSource.next(response);
+      console.log(response);
     }, error => {
       console.log(error);
     });
@@ -40,18 +41,22 @@ export class BasketService {
   addItemToBasket(item: IProduct, quantity = 1) {
     const itemToAdd: IBasketItem = this.mapProductItemToBasketItem(item, quantity);
     const basket = this.getCurrentBasketValue() ?? this.createBasket();
-    basket.items = this.addUpdateItem(basket.items, itemToAdd, quantity);
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
   }
-  addUpdateItem(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number): IBasketItem[] {
-    const index = items.findIndex(i=>i.id === itemToAdd.id);
+
+  private addOrUpdateItem(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number):
+    IBasketItem[] {
+    const index = items.findIndex(i => i.id === itemToAdd.id);
     if (index === -1) {
-itemToAdd.quantity = quantity;
-items.push(itemToAdd);
-    } else{
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    } else {
       items[index].quantity += quantity;
     }
     return items;
   }
+
   private createBasket(): IBasket {
     const basket = new Basket();
     localStorage.setItem('basket_id', basket.id);
@@ -66,7 +71,6 @@ items.push(itemToAdd);
       quantity,
       brand: item.productBrand,
       type: item.productType
-    }
-
+    };
   }
 }
